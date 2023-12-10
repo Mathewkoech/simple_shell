422b4e2278c00fcc5cfee19b11dd2697cb49e6eb
mathewkoech <mathewkoech55@gmail.com>
2023-12-10 04:13:52 -0800

    tokenixer fix

--- tokenizer.c
+++ tokenizer.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * tokenizer - Tokenizes a string into an array of strings.
  * @str: The string to tokenize.
@@ -9,43 +8,42 @@ char **tokenizer(char *str)
 {
 	char *token = NULL, *temp = NULL;
 	char **command = NULL;
-	int count = 0;
-	int i = 0;
+	int count = 0, i = 0, j = 0;
 
 	if (!str)
 	{
 		return (NULL);
 	}
 	temp = strdup(str);
-
 	token = strtok(temp, DELIM);
-	if (!token)
-	{
-		free(str), str = NULL;
-		free(temp), temp = NULL;
-		return (NULL);
-	}
 	while (token)
 	{
 		count++;
-		token = strtok(NULL, DELIM)
+		token = strtok(NULL, DELIM);
 	}
-	free(temp), temp = NULL;
-
+	free(temp);
 	command = malloc(sizeof(char *) * (count + 1));
 	if (!command)
+		return (NULL);
+	token = strtok(str, DELIM);
+	if (!token)
 	{
-		free(str);
+		free(command);
 		return (NULL);
 	}
-	token = strtok(line, DELIM);
 	while (token)
 	{
 		command[i] = strdup(token);
+		if (!command[i])
+		{
+			for (j = 0; j < i; ++j)
+				free(command[j]);
+			free(command);
+			return (NULL);
+		}
 		token = strtok(NULL, DELIM);
 		i++;
 	}
 	command[i] = NULL;
-	free(str);
 	return (command);
 }
