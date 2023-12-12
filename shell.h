0f444259d4d101fe90977ee539d2a7499b45e5f5
ed_ <eddieweks@gmail.com>
2023-12-12 05:55:19 +0300

    updates

--- shell.h
+++ shell.h
@@ -16,6 +16,18 @@
 extern char **environ;
 
 int _strlen(char *s);
+int hsh_exec(char **args, char **env_);
+int hsh_cd(char **args);
+int _strcmp(char *s1, char *s2);
+int _strncmp(char *str1, char *str2, int n);
+char *_strcpy(char *dest, char *src);
+void _exit(char *s);
+void _myprintf(const char *s);
+void _prompt(void);
+char *rmspace(char *cmd);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, const char *src, size_t n);
+void hsh_help(char **args);
 char *read_line(void);
 char **tokenizer(char *str);
 char *get_environ(char *var);
